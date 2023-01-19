05948e15100045ca4262367f1162b355fa540d04
gopalakrishnatelaputta <121857875+gopalakrishnatelaputta@users.noreply.github.com>
2023-01-19 18:55:51 +0530

    Add files via upload

---
+++ LinkedHashMap1.java
@@ -0,0 +1,46 @@
+package collectionsandframework;
+
+import java.util.LinkedHashMap;
+ 
+public class LinkedHashMap1 
+{
+	public static void main(String[] args) 
+	{
+		LinkedHashMap<Integer,String> lhm=new LinkedHashMap<Integer,String>();
+		
+		lhm.put(51, "Gopala krishna");
+		lhm.put(52, "krishna");
+		lhm.put(57, "Sai Ram");
+		lhm.put(53, "Naga raju");
+		lhm.put(56, "Imran");
+		lhm.put(54, "Mahesh");
+		
+		Integer id = 51;
+		
+		if(lhm.containsKey(id))
+		{
+			System.out.println("Customer Found with id   "+id+"Name of the customer is :"+lhm.get(id));
+		}
+		else
+		{
+			System.out.println(id+"id Does not exist");
+		}
+		String name="Naga raju";
+		if(lhm.containsValue(name))
+		{
+			System.out.println();
+			System.out.println("Customer found with name "+name+"  exists in the map");
+			
+		}
+		else {
+			System.out.println();
+			System.out.println("Customer not fount with the name "+name);
+		}
+		id =53;
+		lhm.put(id, "Dinesh");
+		System.out.println();
+		System.out.println(lhm);
+		
+	}
+
+}
